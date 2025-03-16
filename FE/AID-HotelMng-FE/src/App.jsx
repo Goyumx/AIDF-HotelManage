bee3f8f3a90d005ddbaae2c936165555f542e32c
Goyumx <goyum.doingcode@gmail.com>
2025-03-16 20:36:11 +0530

    shadcn element config

--- FE/AID-HotelMng-FE/src/App.jsx
+++ FE/AID-HotelMng-FE/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   
@@ -6,6 +8,8 @@ function App() {
     <>
       <div>
         <h1 className="text-blue-500 text-2xl mt-4">Tailwind</h1>
+        <Button variant="outline">Button</Button>
+
       </div>
     </>
   )
