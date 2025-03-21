57d61740810cf7cd867db2ba6639f72dbb5b7c76
Goyumx <goyum.doingcode@gmail.com>
2025-03-21 20:50:53 +0530

    User and Booking BE API done"

--- BE/src/index.js
+++ BE/src/index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import hotelsRouter from "./api/hotel.js";
 import connectDB from "./infrastructure/db.js";
+import usersRouter from "./api/user.js";
+import bookingsRouter from "./api/booking.js";
 
 const app = express();
 
@@ -10,5 +12,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/hotels", hotelsRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/bookings", bookingsRouter);
 
 app.listen(8000, console.log("Server Running"));
