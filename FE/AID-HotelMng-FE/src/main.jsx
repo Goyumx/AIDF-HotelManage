b76845bbc6153e5358ca8fb8c9be4b8c19c2100e
Goyumx <goyum.doingcode@gmail.com>
2025-03-22 04:38:53 +0530

    Sample pages and Nav bar as Layout

--- FE/AID-HotelMng-FE/src/main.jsx
+++ FE/AID-HotelMng-FE/src/main.jsx
@@ -4,12 +4,19 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import HomePage from "./pages/home.page";
+import SignInPage from './pages/sign-in.page'
+import SignUpPage from './pages/sign-up.page'
+import RootLayout from "./layouts/root-layout.layout";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Route>
     </Routes>
     </BrowserRouter>
   </StrictMode>,
