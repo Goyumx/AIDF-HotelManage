ba8983ba65e60859794ddf0e40cda64b6cdf9567
Goyumx <goyum.doingcode@gmail.com>
2025-03-25 13:50:49 +0530

    feat: added create hotel form and configured

---
+++ FE/AID-HotelMng-FE/src/components/CreateHotelForm.jsx
@@ -0,0 +1,182 @@
+import { useCreateHotelMutation } from "@/lib/api";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input"
+  import { Textarea } from "@/components/ui/textarea"
+
+  const formSchema = z.object({
+    name: z.string().min(1, { message: "Hotel name is required" }),
+    location: z.string().min(1),
+    rating: z.number().min(1).max(5),
+    reviews: z.number(),
+    image: z.string().min(1),
+    price: z.number(),
+    description: z.string().min(1),
+  });
+
+
+export default function CreateHotelPage() {
+
+    const [createHotel, { isLoading }] = useCreateHotelMutation();
+  
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+    })
+
+    const handleSubmit= async (values) => {
+        const { name, location, image, rating, reviews, price, description } = values;
+        try {
+          toast.loading("Creating hotel...");
+          await createHotel({
+            name,
+            location,
+            rating,
+            reviews,
+            image,
+            price,
+            description,
+          }).unwrap();
+          toast.success("Hotel created successfully");
+        } catch (error) {
+          toast.error("Hotel creation failed");
+        }
+        console.log(values);
+    };
+
+  return (
+    <Form {...form}>
+      <form className="w-1/2" onSubmit={form.handleSubmit(handleSubmit)}>
+        <div className="grid gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Hotel Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Hotel Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input placeholder="Location" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="rating"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Rating</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Rating"
+                    onChange={(e) => {
+                      field.onChange(parseFloat(e.target.value));
+                    }}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="reviews"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Reviews</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Reviews"
+                    onChange={(e) => {
+                      field.onChange(parseFloat(e.target.value));
+                    }}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <Input placeholder="Image" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Price"
+                    onChange={(e) => {
+                      field.onChange(parseFloat(e.target.value));
+                    }}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Description" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="mt-4">
+          <Button type="submit">Create Hotel</Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
