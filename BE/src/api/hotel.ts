61d138b4177182cd2d12e53c37266a0a899a2dd3
Goyumx <goyum.doingcode@gmail.com>
2025-03-26 23:12:35 +0530

    feat:AI based hotel search query in BE

--- BE/src/api/hotel.ts
+++ BE/src/api/hotel.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { isAuthenticated } from "./middleware/authentication-middleware";
 import { isAdmin } from "./middleware/authorization-middleware";
 import { createEmbeddings } from "../application/embeddings";
+import { retrieve } from "../application/retrieve";
 
 const hotelsRouter = express.Router();
 
@@ -12,8 +13,8 @@ hotelsRouter
   .get(getHotelById)
   .put(updateHotel)
   .delete(deleteHotel);
-hotelsRouter.route("/llm").post(generateResponse);
 hotelsRouter.route("/embeddings/create").post(createEmbeddings);
+hotelsRouter.route("/search/retrieve").get(retrieve);
 
 
 export default hotelsRouter; 
